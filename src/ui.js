e7c9aa2c8d1a515ed584eb8e4baffc9444d800ce
Jrad7 <jared.w.wright@gmail.com>
2015-09-17 21:21:24 -0500

    fix jquery css
    Signed-off-by: Jrad7 <jared.w.wright@gmail.com>

--- src/ui.js
+++ src/ui.js
@@ -4,7 +4,7 @@ var UI = function() {
                .html('<iframe style="border: 0px; " src="" width="100%" height="100%"></iframe>')
                .dialog({
                    autoOpen: false,
-                   modal: true,
+                   modal: false,
                    height: 400,
                    width: 400,
                    title: "Salt Bot UI"
